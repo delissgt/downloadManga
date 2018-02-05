ceecda3d54c5b78a66e4ebb5f17b50ba5c280255
delissgt <delissgt@gmail.com>
2018-02-05 15:06:55 -0600

    get chapter number with regex

--- MangaDeliss.py
+++ MangaDeliss.py
@@ -3,17 +3,13 @@ from PyQt4.QtGui import QMainWindow, QApplication
 import requests, bs4
 from PyQt4.QtCore import QThread
 from PyQt4.QtCore import pyqtSignal as Signal
+import re
+
                                 #jalar interfaz
 class VentanaMain(QMainWindow,DescManga.Ui_MainWindow):
     def __init__(self, parent=None):
         super(VentanaMain, self).__init__(parent)
         self.setupUi(self) #jalar interfaz
-
-
-
-        #res = requests.get('http://mangachameleon.com/?c=545b812745b9efab4343f776&i=4e70ea03c092255ef70046f0')  # accedo a la web
-
-
 
         self.pushBoton_stop.clicked.connect(self.funcionStop)
         self.pushBoton_start.clicked.connect(self.funcionStart)
@@ -26,13 +22,12 @@ class VentanaMain(QMainWindow,DescManga.Ui_MainWindow):
         miHilo = ClaseHilo(miLink, self) #paso valores que puedo obtener#CREAR INSTANCIA
 
         miHilo.datos_listos.connect(self.colocarDatos)
-
         miHilo.start() #lanzo mi hilo
 
-    def colocarDatos(self, miTitulo, cadena_hola):
+    def colocarDatos(self, miTitulo, micapitulo):
 
         self.label_titulo.setText(miTitulo[0].get_text())# titulo
-        self.label_capitulo.setText(cadena_hola)
+        self.label_capitulo.setText(micapitulo)
 
 
 
@@ -48,11 +43,19 @@ class ClaseHilo(QThread): #obetner to-do de la web operaciones que no depende de
     def run(self):#obtener datos de la paginaWeb
         #str(self.link) o unicode(self.link)
         noStarchSoup = bs4.BeautifulSoup(self.link.text, "lxml")  # accedo al codigo fuente
-        elems = noStarchSoup.select('h2 > a')  # busco el elemto dando esta especificacion ('h2 > a')
-        # elems[0].get_text()  # obetengo el texto <h2><a>Naruto</a></h2> ---> Naruto
-       # miTitulo=elems[0].get_text()
-        #print(miTitulo)
-        self.datos_listos.emit(elems, 'HOla')
+
+        obtener_titulo = noStarchSoup.select('h2 > a')  # busco el elemto dando esta especificacion ('h2 > a')
+
+
+        get_texto_cap = noStarchSoup.select('option[selected="selected"]')  # obtener numero captitulo
+        texto = get_texto_cap[0].get_text()
+        expresion_regular = re.compile(r'(\d)?(\d)?\d(\.)?(\d)?(\d)?(\d)?')
+        cap = expresion_regular.search(texto)
+        capitulo = cap.group()
+
+
+
+        self.datos_listos.emit(obtener_titulo, capitulo)
 
 
 
